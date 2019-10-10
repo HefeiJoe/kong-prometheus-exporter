b6d819331da65378f48d0f17ebfaddfc06c59add
何飞 <894598156@qq.com>
2019-10-10 15:01:56 +0800

    Update kongPrometheusExporter.go

--- collector/kongPrometheusExporter.go
+++ collector/kongPrometheusExporter.go
@@ -10,19 +10,6 @@ import (
 	"strconv"
 	"strings"
 )
-const (
-	BANDWIDTH = "kong_bandwidth"
-	DATASTORE_REACHABLE = "kong_datastore_reachable"
-	CONSUMER_HTTP_STATUS = "kong_consumer_http_status"
-	ROUTE_HTTP_STATUS = "kong_route_http_status"
-	LATENCY_BUCKET = "kong_latency_bucket"
-	LATENCY_COUNT = "kong_latency_count"
-	LATENCY_SUM = "kong_latency_sum"
-	MEMORY_LUA_SHARED_DICT_TOTAL_BYTES = "kong_memory_lua_shared_dict_total_bytes"
-	NGINX_HTTP_CURRENT_CONNECTIONS = "kong_nginx_http_current_connections"
-	NGINX_METRIC_ERRORS_TOTAL = "kong_nginx_metric_errors_total"
-
-)
 type Exporter struct {
 	bandwidth prometheus.GaugeVec
 	datastore_reachable prometheus.Gauge
@@ -175,7 +162,7 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 							}
 						}
 						newResponse = newResponse + k + " " + strconv.FormatFloat(v, 'E', -1, 64) + "\n"
-						kMap := StrToMap(k)
+						kMap := libs.StrToMap(k)
 						if strings.Contains(k,_const.BANDWIDTH){
 							e.bandwidth.WithLabelValues(kMap["type"], kMap["service"], kMap["route"]).Set(v)
 						}
