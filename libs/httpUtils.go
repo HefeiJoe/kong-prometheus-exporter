95f6d6e02201840c6234750dda5336456fb6a6af
何飞 <894598156@qq.com>
2019-10-17 09:40:13 +0800

    Update httpUtils.go

--- libs/httpUtils.go
+++ libs/httpUtils.go
@@ -3,15 +3,21 @@ package libs
 import (
 	"io/ioutil"
 	"net/http"
+	"github.com/wonderivan/logger"
 )
 
 func Get(url string) (response string) {
 	client := &http.Client{}
 	resp, err := client.Get(url)
 	if err != nil {
-		panic(err)
+		logger.Error(err)
+		return ""
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error(err)
+		return ""
+	}
 	return string(body)
 }
